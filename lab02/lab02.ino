cb27cdd0f91296c5a91779b9f2d947ffd5b05246
xiaoshen-zjy <1092878965@qq.com>
2026-06-19 19:52:58 +0800

    lab02：完成基础LED闪烁代码

---
+++ lab02/lab02.ino
@@ -0,0 +1,17 @@
+// lab02 实验2 基础IO LED闪烁
+const int ledPin = 2; // ESP32板载LED引脚
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+  Serial.println("实验2运行中");
+}
+
+void loop() {
+  digitalWrite(ledPin, HIGH);
+  Serial.println("LED亮");
+  delay(1000); // 亮1秒
+  digitalWrite(ledPin, LOW);
+  Serial.println("LED灭");
+  delay(1000); // 灭1秒
+}
